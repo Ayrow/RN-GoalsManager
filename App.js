3831a58e5ff4ecb2b9b12bae394b8b5e3789abdf
Aymeric Pilaert <aymericpilaert@ordinateur-portable-de-aymeric.home>
2022-12-21 11:43:58 +0100

    split up GoalItem to a component

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import {
   StyleSheet,
-  Text,
   View,
   TextInput,
   Button,
   FlatList,
 } from 'react-native';
+
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -39,9 +40,7 @@ export default function App() {
           data={courseGoals}
           renderItem={(itemData) => {
             return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
+              
             );
           }}
           keyExtractor={(item, index) => {
@@ -79,13 +78,5 @@ const styles = StyleSheet.create({
   goalsContainer: {
     flex: 5,
   },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white',
-  },
+  
 });
