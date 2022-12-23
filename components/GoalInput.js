72a8a1849ce8efcc9b7680554d7ee19fce6be448
Aymeric Pilaert <aymericpilaert@ordinateur-portable-de-aymeric.home>
2022-12-23 13:28:57 +0100

    added image and changed color background

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,11 @@
-import { StyleSheet, TextInput, View, Modal, Button } from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Modal,
+  Button,
+  Image,
+} from 'react-native';
 
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -15,6 +22,10 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType='slide'>
       <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/goal.png')}
+        />
         <TextInput
           style={styles.textInput}
           placeholder='Your course goal'
@@ -26,7 +37,7 @@ function GoalInput(props) {
             <Button title='Add goal' onPress={addGoalHandler} />
           </View>
           <View style={styles.button}>
-            <Button title='Cancel' />
+            <Button title='Cancel' onPress={props.onCancel} />
           </View>
         </View>
       </View>
@@ -41,10 +52,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 24,
     padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
+    backgroundColor: '#311b6b',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
   textInput: {
     borderWidth: 1,
